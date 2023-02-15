8ea83978aab5c0e5468f0c8c5f3c20787d82c8f8
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-15 17:58:05 -0500

    singleton

---
+++ Design Patterns/SigletonPattern.js
@@ -0,0 +1,28 @@
+function Process(state) {
+  this.state = state;
+}
+
+const Singleton = (function () {
+  function ProcessManager() {
+    this.numProcess = 0;
+  }
+  let instancePManager;
+  function createProcessManager() {
+    instancePManager = new ProcessManager();
+    return instancePManager;
+  }
+  return {
+    getInstance: function () {
+      if (!instancePManager) {
+        instancePManager = createProcessManager();
+      }
+      return instancePManager;
+    },
+  };
+})();
+
+const ProcessManager = Singleton.getInstance();
+const ProcessManager2 = Singleton.getInstance();
+
+console.log(ProcessManager);
+console.log(ProcessManager === ProcessManager2);
